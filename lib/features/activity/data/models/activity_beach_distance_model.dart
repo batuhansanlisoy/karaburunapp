6553ee56b58660bf946594c24aff3c359366ca37
Batuhan Şanlısoy <batuhan.sanlisoy@hotmail.com>
2026-01-31 02:04:54 +0300

    fix

--- lib/features/activity/data/models/activity_beach_distance_model.dart
+++ lib/features/activity/data/models/activity_beach_distance_model.dart
@@ -1,7 +1,7 @@
 class ActivityBeachDistance {
   final int id;
   final int activityId;
-  final int organizationId;
+  final int beachId;
   final double distanceMeter;
   final DateTime createdAt;
   final DateTime updatedAt;
@@ -9,7 +9,7 @@ class ActivityBeachDistance {
   ActivityBeachDistance({
     required this.id,
     required this.activityId,
-    required this.organizationId,
+    required this.beachId,
     required this.distanceMeter,
     required this.createdAt,
     required this.updatedAt,
@@ -20,7 +20,7 @@ class ActivityBeachDistance {
     return ActivityBeachDistance(
       id: json['id'] as int,
       activityId: json['activity_id'] as int,
-      organizationId: json['organization_id'] as int,
+      beachId: json['beach_id'] as int,
       distanceMeter: (json['distance_meter'] as num).toDouble(),
       createdAt: DateTime.parse(json['created_at'] as String),
       updatedAt: DateTime.parse(json['updated_at'] as String),
