e8ba2c1237b0a42280efcc002aa43aa9ae3184a7
Batuhan Şanlısoy <batuhan.sanlisoy@hotmail.com>
2026-02-22 18:15:09 +0300

    phone formatter

--- lib/core/helpers/string_helpers.dart
+++ lib/core/helpers/string_helpers.dart
@@ -7,4 +7,22 @@ extension StringHelpers on String {
   String capitalizeAll() {
     return split(" ").map((word) => word.capitalize()).join(" ");
   }
+
+  String formatPhoneNumber() {
+    // Önce içindeki tüm boşlukları ve gereksiz karakterleri temizle
+    String cleanNumber = replaceAll(RegExp(r'\D'), '');
+
+    // Eğer numara 0 ile başlıyorsa (0535 gibi), baştaki 0'ı atalım ki standart olsun
+    if (cleanNumber.startsWith('0')) {
+      cleanNumber = cleanNumber.substring(1);
+    }
+
+    // Eğer elimizde tam 10 hane kaldıysa formatla (5350604836 -> 535 060 48 36)
+    if (cleanNumber.length == 10) {
+      return "${cleanNumber.substring(0, 3)} ${cleanNumber.substring(3, 6)} ${cleanNumber.substring(6, 8)} ${cleanNumber.substring(8, 10)}";
+    }
+
+    // Eğer hane sayısı tutmuyorsa ham veriyi döndür (Hata vermesin diye)
+    return this;
+  }
 }
